732a71beeb32b293e3e696ff0a8eadda6f4aeb16
raguma88 <35690463+raguma88@users.noreply.github.com>
2018-03-04 22:08:48 +0300

    Упражнение 2

---
+++ Sandbox/src/main/java/my/homeworks/Distance.java
@@ -0,0 +1,15 @@
+package my.homeworks;
+
+public class Distance {
+
+  public static void main(String[] args) {
+
+    Point p1 = new Point(1, 1);
+
+    Point p2 = new Point(4,5);
+
+    System.out.println("Расстояние между точками p1 и p2 = " +  p1.distance(p2));
+
+  }
+
+}
