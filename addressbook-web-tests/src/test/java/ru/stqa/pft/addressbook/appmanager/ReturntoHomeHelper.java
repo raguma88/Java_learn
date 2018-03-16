00781754a4182c52ff1bc0f5a9c059ea7f56470f
raguma88 <35690463+raguma88@users.noreply.github.com>
2018-03-16 11:43:42 +0300

    Задание 5 Создание BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ReturntoHomeHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ReturntoHomeHelper.java
@@ -3,14 +3,15 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ReturntoHomeHelper {
+public class ReturntoHomeHelper extends HelperBase {
   private FirefoxDriver wd;
 
   public ReturntoHomeHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
+
+    click(By.linkText("home page"));
   }
 }
