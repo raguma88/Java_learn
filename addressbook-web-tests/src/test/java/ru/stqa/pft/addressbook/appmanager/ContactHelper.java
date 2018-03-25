468ae39d0033ee02dba895cdc451f5b352ca63be
raguma88 <35690463+raguma88@users.noreply.github.com>
2018-03-25 11:24:33 +0300

    Удаление лишних файлов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,12 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-
-import java.util.NoSuchElementException;
 
 public class ContactHelper extends HelperBase {
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -10,11 +10,13 @@ public class ContactModificationTests extends TestBase {
     app.getNavigationHelper().gotoHomePage();
     app.getNavigationHelper().gotoHomePage();
     if (! app.getContactHelper().isThereAContact()) {
-      app.getContactHelper().createContact(new ContactData("Gulnara", "Rafikova", null, null, "test1"), true);
+      app.getContactHelper().createContact(new ContactData("Gulnara", "Rafikova", null, null, "test1"),
+              true);
     }
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("Gulnara", "Rafikova", "Russia, RT, Kazan", "89179151010", "test1"), false);
+    app.getContactHelper().fillContactForm(new ContactData("Gulnara", "Rafikova", "Russia, RT, Kazan", "89179151010",
+            "test1"), false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
   }
