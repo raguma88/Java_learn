cd14584ba1f9e644b94a1f8b04ed996ea3401dc7
raguma88 <35690463+raguma88@users.noreply.github.com>
2018-04-01 21:42:32 +0300

    Лекции 4.6 - 4.9 для контактов (создание, модификация без сортировки)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -8,7 +8,7 @@ public class GroupData {
 
 
   public GroupData(String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -56,12 +56,15 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
+    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    return name != null ? name.hashCode() : 0;
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
 
 }
