a4f326bba95e53650d29f2e3988ffadb4dbffd27
raguma88 <35690463+raguma88@users.noreply.github.com>
2018-06-20 21:11:18 +0300

    Задание 15 для групп. Проверки списков, загружаемых из БД

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -53,9 +53,9 @@ public class GroupCreationTests extends TestBase {
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after.size(), equalTo(before.size() + 1));
 
     assertThat(after, equalTo
