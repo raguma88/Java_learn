bc5f04ec3fc6e2121f4acfb4667debcb704f9ee8
raguma88 <35690463+raguma88@users.noreply.github.com>
2018-07-07 20:27:25 +0300

    Лекция 8.7. Получение почты на внешний почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -16,19 +16,21 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-  @BeforeMethod
+  //@BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
 
   @Test
-  public void testRegistration() throws IOException, MessagingException {
+  public void testRegistration() throws IOException, MessagingException, javax.mail.MessagingException {
     long now = System.currentTimeMillis();
     String user = String.format("user%s", now);
     String email = String.format("user%s@localhost.localdomain", now);
     String password = "password";
+    app.james().createUser(user, password);
     app.registration().start(user, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 80000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user, password));
@@ -42,7 +44,7 @@ public class RegistrationTests extends TestBase {
   }
 
 
-  @AfterMethod(alwaysRun = true)
+  //@AfterMethod(alwaysRun = true)
   public void stopMailServer() {
     app.mail().stop();
   }
